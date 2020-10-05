20fdb2d36f41185e404ce65ba81e134787992615
Nilesh <patil_nilesh55@ymail.com>
2020-10-05 23:06:05 +0530

    Simple Program

---
+++ SimpleProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students s = new Students();
+            s.Id = 1;
+            s.Name = "nilesh";
+            s.Introduction();
+            Console.ReadKey();
+        }
+    }
+
+    
+}
---
+++ SimpleProgram/Students.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleProgram
+{
+    class Students
+    {
+        public int Id;
+        public string Name;
+
+        public void Introduction()
+        {
+            Console.WriteLine("My name is:" + Name + "and Id is" + Id);
+        }
+    }
+}
